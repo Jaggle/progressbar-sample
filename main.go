a6fba7ae7860daa7c6bd096ac62acbc913a52858
jaggle <singviy@gmail.com>
2020-07-31 18:25:04 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    progressbar "progressbar/bar"
+    "time"
+)
+
+func main() {
+    var bar progressbar.Bar
+    bar.NewOption(0, 100)
+
+    for i := 0; i <= 100; i++ {
+        time.Sleep(100 * time.Millisecond)
+        bar.Play(int64(i))
+    }
+    bar.Finish()
+}
